a5a388c7e54b92bec37df9b4d86ff790f893cf47
Erik Wilson <Erik.E.Wilson@gmail.com>
2018-10-16 11:40:29 -0700

    generated from go-skel

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/rancher/kube-api-auth
+
+require (
+	github.com/Sirupsen/logrus v0.10.0 // indirect
+	github.com/sirupsen/logrus v1.1.1
+	github.com/urfave/cli v1.18.0
+)
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+	"github.com/urfave/cli"
+)
+
+var VERSION = "v0.0.0-dev"
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "kube-api-auth"
+	app.Version = VERSION
+	app.Usage = "You need help!"
+	app.Action = func(c *cli.Context) error {
+		logrus.Info("I'm a turkey")
+		return nil
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		logrus.Fatal(err)
+	}
+}
